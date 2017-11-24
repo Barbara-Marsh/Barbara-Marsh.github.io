923ba8ad2ad860cbeccd87806f9e5a24d78e2e63
Barbara Marsh <barbara.marsh@live.vu.edu.au>
2017-11-25 02:23:17 +1100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,8 +6,8 @@ var app = angular.module('myApp', ['ngRoute']);
 
 app.config(function ($routeProvider) {
     $routeProvider
-        .when('/', { templateUrl: 'templates/home.html' })
-        .when('/about', { templateUrl: 'templates/about.html' })
+        .when('/', { templateUrl: '../templates/home.html' })
+        .when('/about', { templateUrl: '../templates/about.html' })
         .otherwise({ redirectTo: "/" });
 })
     .controller('AppCtrl', function ($scope) {
